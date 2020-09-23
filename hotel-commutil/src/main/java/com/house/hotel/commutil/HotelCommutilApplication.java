e0d71b86e41d914e24ee1c6205e9e4488ca89b5e
“muhao.zou” <327618111@qq.com>
2020-09-23 23:23:03 +0800

    初始化项目

---
+++ hotel-commutil/src/main/java/com/house/hotel/commutil/HotelCommutilApplication.java
@@ -0,0 +1,13 @@
+package com.house.hotel.commutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelCommutilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelCommutilApplication.class, args);
+    }
+
+}
---
+++ hotel-commutil/src/test/java/com/house/hotel/commutil/HotelCommutilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.house.hotel.commutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelCommutilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
