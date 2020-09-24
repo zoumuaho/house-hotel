72335f9fb53c42496df05797a32a5aa3354a5561
“muhao.zou” <327618111@qq.com>
2020-09-24 23:43:28 +0800

    初始化项目

--- hotel-webapi/src/main/java/com/house/hotel/webapi/HotelWebapiApplication.java
+++ hotel-webapi/src/main/java/com/house/hotel/webapi/HotelWebapiApplication.java
@@ -2,8 +2,11 @@ package com.house.hotel.webapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+
+@MapperScan(basePackages = "com.house.hotel.dao.mapper.*")
 public class HotelWebapiApplication {
 
 	public static void main(String[] args) {
--- hotel-webapi/src/test/java/com/house/hotel/webapi/HotelWebapiApplicationTests.java
+++ hotel-webapi/src/test/java/com/house/hotel/webapi/HotelWebapiApplicationTests.java
@@ -1,13 +1,28 @@
 package com.house.hotel.webapi;
 
+import com.house.hotel.dao.entity.HotelUserInfo;
+import com.house.hotel.dao.mapper.HotelUserInfoMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import tk.mybatis.mapper.common.Mapper;
 
-@SpringBootTest
+import java.util.List;
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = HotelWebapiApplication.class)
 class HotelWebapiApplicationTests {
+	@Autowired
+	private HotelUserInfoMapper hotelUserInfoMapper;
 
 	@Test
 	void contextLoads() {
+		List<HotelUserInfo> hotelUserInfoList =  hotelUserInfoMapper.selectAll();
+		hotelUserInfoList.forEach(v->{
+			System.out.println(v.getUserName());
+		});
 	}
 
 }
